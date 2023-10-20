9a7e33bc1f309da4683de0d884ea7bd91f0f8953
KostiantynZhulinas <k.zhulinas@gmai.com>
2023-10-21 00:25:50 +0300

    add files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-function Student(name, surname, birthYear, course) {
-    this.name = name;
-    this.surname = surname;
+function Student(firstName, lastName, birthYear, course) {
+    this.firstName = firstName;
+    this.lastName = lastName;
     this.birthYear = birthYear;
     this.course = course;
     this.grades = [];
@@ -19,17 +19,17 @@ Student.prototype.getAverageGrade = function () {
     if (this.grades.length === 0) {
         return 0;
     }
-    var sum = this.grades.reduce(function (a, b) {
+    let sum = this.grades.reduce(function (a, b) {
         return a + b;
     });
-    return sum / this.grades.length;
+    let averageGrade = sum / this.grades.length;
+    return averageGrade.toFixed(1);
 }
-
 Student.prototype.getAverageAttendance = function () {
     if (this.attendance.length === 0) {
         return 0;
     }
-    var sum = this.attendance.reduce(function (a, b) {
+    let sum = this.attendance.reduce(function (a, b) {
         return a + b;
     });
     return sum / this.attendance.length;
@@ -44,31 +44,30 @@ Student.prototype.changeCourse = function (newCourse) {
 }
 
 Student.prototype.getInformation = function () {
-    return "Name: " + this.name + "\nSurname: " + this.surname + "\nBirth Year: " + this.birthYear + "\nCourse: " + this.course + "\nGrades: " + this.grades.join(", ") + "\nAttendance: " + this.attendance.join(", ");
-}
+    return{
+        firstName: this.firstName,
+        lastName: this.lastName,
+        birthYear: this.birthYear,
+        course: this.course,
+        grades: this.grades,
+        attendance: this.attendance,
+    };
+    }      
 Student.prototype.getAverageAttendancePercentage = function () {
     if (this.attendance.length === 0) {
         return "No attendance data available.";
     }
     
-    var sum = this.attendance.reduce(function (a, b) {
+    let sum = this.attendance.reduce(function (a, b) {
         return a + b;
     });
     
-    var averageAttendancePercentage = (sum / this.attendance.length) * 100;
+    let averageAttendancePercentage = (sum / this.attendance.length) * 100;
     
     return averageAttendancePercentage.toFixed(1) + "%";
 }
 
-function JuniorStudent(name, surname, birthYear, course, gradeCount, classCount) {
-    Student.call(this, name, surname, birthYear, course);
-    this.grades = Array(gradeCount).fill(0);
-    this.attendance = Array(classCount).fill(0);
-}
-
-JuniorStudent.prototype = Object.create(Student.prototype);
-
-var student = new JuniorStudent("Kostiantyn", "Zhulinas", 1994, 1, 0, 0);
+let student = new Student("Kostiantyn", "Zhulinas", 1994, 1, 0, 0);
 
 student.addGrade(100);
 student.addGrade(0);
@@ -79,6 +78,6 @@ student.addAttendance(0);
 
 
 console.log(student.getInformation());
-console.log("Average Grade: " + student.getAverageGrade().toFixed(1));
+console.log("Average Grade: " + student.getAverageGrade());
 console.log("Average Attendance: " + student.getAverageAttendancePercentage());
 console.log("Number of Attended Classes: " + student.getNumberOfClasses());
